24c67814661b572b37de04b6698e4c21a46d1579
Noel Moreno Lemus <nmlemus@gmail.com>
2023-12-21 00:29:31 -0500

    Update Dec21

--- uqsim_ui/gui/sidebar.py
+++ uqsim_ui/gui/sidebar.py
@@ -1,14 +1,22 @@
 from PIL import Image
 
-image = Image.open('/Users/macbook12/research/uqsimpy/assets/logo.png')
+image = Image.open("logo.png")
 
 
 def set_sidebar(st):
     st.sidebar.image(image, width=180)
-    st.sidebar.title('UQSim')
+    st.sidebar.title("UQSim")
 
-    objective = st.sidebar.radio('',
-                                 ('Create Model', 'Simulate', 'Uncertainty Quantification', 'Parameter Estimation',
-                                  'Sensitivity Analysis', 'Info'))
+    objective = st.sidebar.radio(
+        "",
+        (
+            "Create Model",
+            "Simulate",
+            "Uncertainty Quantification",
+            "Parameter Estimation",
+            "Sensitivity Analysis",
+            "Info",
+        ),
+    )
 
     return objective
